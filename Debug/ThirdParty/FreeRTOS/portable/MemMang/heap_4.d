6c91556b0df0343ddcc0552ce05aad8f0219dbed
kjlau0112 <lkarnjye@gmail.com>
2021-11-27 17:13:02 +0800

    Initial Buildable project for freeRTOS STM32F407 using STM32CUbeIDE

---
+++ Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,29 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/portable.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/list.h
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/projdefs.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/portable.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/list.h:
---
+++ Debug/ThirdParty/FreeRTOS/queue.d
@@ -0,0 +1,34 @@
+ThirdParty/FreeRTOS/queue.o: ../ThirdParty/FreeRTOS/queue.c \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/portable.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/list.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/queue.h \
+ D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/projdefs.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/portable.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/list.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/queue.h:
+
+D:/STM32CubeIDE/Users/User/STM32CubeIDE/workspace_1.1.0/Udemy_FreeRTOS/RTOS_Workspace/STM32F407_RTOS_project/ThirdParty/FreeRTOS/include/task.h:
